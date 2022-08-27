84de9270f7b700802a009a887393049efa654bd6
manhkkkk <pktopad@gmail.com>
2022-08-27 20:04:38 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "antd/dist/antd.css";
+import 'antd/dist/antd.min.css';
 import { Provider } from "react-redux";
 import { store } from './redux/store'
 ReactDOM.render(
